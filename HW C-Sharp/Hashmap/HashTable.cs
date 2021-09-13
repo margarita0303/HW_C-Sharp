de487cfbc8ec9f7c8bb1e721ff22103a39f7cf8f
Margarita <lashina.2000@gmail.com>
2021-09-14 01:25:10 +0300

    Initial commit

---
+++ HW C-Sharp/Hashmap/HashTable.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+
+namespace HashTable
+{
+    public class Item<TK, TV>
+    {
+        public TK Key;
+        public TV Value;
+        
+        public Item(TK key, TV value)
+        {
+            Key = key;
+            Value = value;
+        }
+    }
+    
+    public class HashTable<TK, TV>
+    {
+        private int Size;
+        private Dictionary<int, LinkedList<Item<TK, TV>>> _items;
+
+        public HashTable(int maxSize = 256 - 1)
+        {
+            Size = maxSize;
+            _items = new Dictionary<int, LinkedList<Item<TK, TV>>>(Size);
+        }
+
+        private int GetHash(TK key)
+        {
+            return key.GetHashCode() % Size;
+        }
+
+        public void Insert(TK key, TV value)
+        {
+            var hash = GetHash(key);
+            var item = new Item<TK, TV>(key, value);
+            if (_items.ContainsKey(hash))
+            {
+                _items[hash].AddLast(item);
+            }
+            else
+            {
+                var itemsList = new LinkedList<Item<TK, TV>>();
+                itemsList.AddLast(item);
+                _items.Add(hash, itemsList);
+            }
+        }
+        
+        public void Delete(TK key)
+        {
+            var hash = GetHash(key);
+            if (!_items.ContainsKey(hash))
+            {
+                return;
+            }
+            var itemsList = _items[hash];
+            foreach (var item in itemsList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    itemsList.Remove(item);
+                    break;
+                }
+            }
+        }
+        
+        public TV Find(TK key)
+        {
+            var hash = GetHash(key);
+            if (!_items.ContainsKey(hash))
+            {
+                throw new KeyNotFoundException();
+            }
+            var itemsList = _items[hash];
+            if (itemsList != null)
+            {
+                foreach (var item in itemsList)
+                {
+                    if (item.Key.Equals(key))
+                    {
+                        return item.Value;
+                    }
+                }
+            }
+            
+            throw new KeyNotFoundException();
+        }
+    }
+}
---
+++ HW C-Sharp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using HashTable;
+
+namespace HW_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hashTable = new HashTable<int,string>();
+            hashTable.Insert(12, "Value1");
+            
+            try
+            {
+                var result = hashTable.Find(12);
+                Console.WriteLine(result);
+            }
+            catch (KeyNotFoundException)
+            {
+                Console.WriteLine("Key can not be found.");
+            }
+            
+            try
+            {
+                var result = hashTable.Find(23);
+                Console.WriteLine(result);
+            }
+            catch (KeyNotFoundException)
+            {
+                Console.WriteLine("Key can not be found.");
+            }
+            
+            hashTable.Insert(20, "Value2");
+            hashTable.Delete(20);
+            
+            try
+            {
+                var result = hashTable.Find(20);
+                Console.WriteLine(result);
+            }
+            catch (KeyNotFoundException)
+            {
+                Console.WriteLine("Key can not be found.");
+            }
+        }
+    }
+}
