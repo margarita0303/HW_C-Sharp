714aff685745d0fd227d8e8961f9500a8597d5e3
Margarita <lashina.2000@gmail.com>
2021-10-05 18:46:53 +0300

    + task sorting hamsters

--- HW C-Sharp/Program.cs
+++ HW C-Sharp/Program.cs
@@ -5,6 +5,7 @@ using HashTable;
 using Simplifier;
 using Palindrome;
 using ConversionOperators;
+using SortingHamsters;
 
 namespace HW_C_Sharp
 {
@@ -18,6 +19,26 @@ namespace HW_C_Sharp
             {
                 Console.WriteLine("Tests for conversion operators have been passed");
             }
+            
+            var sorter = new SorterForHamsters();
+            Console.WriteLine("Not sorted hamsters");
+            sorter.Print();
+            sorter.SortHamsters();
+            Console.WriteLine("Sorted hamsters");
+            sorter.Print();
+
+            // Hamster h1 = new Hamster(2, 30, 62, "gr", "sef");
+            // Hamster h2 = new Hamster(3, 29, 62, "gr", "sef");
+            // Hamster h3 = new Hamster();
+            // Hamster h4 = new Hamster();
+            // h3.Print();
+            // h4.Print();
+            // Console.WriteLine(h3 < h4);
+            // Console.WriteLine(h1 > h2);
+            // Console.WriteLine(h1 <= h2);
+            // Console.WriteLine(h1 >= h2);
+            // Console.WriteLine(h1 == h2);
+            // Console.WriteLine(h1 != h2);
         }
     }
 }
