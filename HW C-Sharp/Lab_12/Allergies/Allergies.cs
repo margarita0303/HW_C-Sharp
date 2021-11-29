c59f1869e1ec7e8fb3f91089410e68234382af06
Margarita <lashina.2000@gmail.com>
2021-11-30 01:18:07 +0300

    + task allergies

---
+++ HW C-Sharp/Lab_12/Allergies/Allergies.cs
@@ -0,0 +1,229 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Allergies
+{
+    enum Allergen
+    {
+        Eggs = 1, 
+        Peanut = 2,
+        Shellfish = 4, 
+        Strawberries = 8,
+        Tomatoes = 16, 
+        Chocolate = 32, 
+        Pollen = 64, 
+        Cats = 128
+    }
+    
+    public class Allergies
+    {
+        private string _name;
+        public string Name
+        {
+            get => _name;
+        }
+        private int _score;
+        public int Score
+        {
+            get => _score;
+        }
+        
+        private List<string> _allegries;
+        
+        public Allergies(string name)
+        {
+            _name = name;
+            _allegries = new List<string>();
+            _score = 0;
+        }
+        
+        public Allergies(string name, int score)
+        {
+            _name = name;
+            _allegries = ListOfAllergiesByScore(score);
+            _score = score;
+        }
+        
+        public Allergies(string name, string allergiesInString)
+        {
+            _name = name;
+            _allegries = ListOfAllergiesByString(allergiesInString);
+            _score = CountScore();
+        }
+
+        public string ToString()
+        {
+            if (_allegries.Count == 0)
+            {
+                return $"{_name} has no allergies";
+            }
+            
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"{_name} is allergic to ");
+
+            for (int i = 0; i < _allegries.Count; i++)
+            {
+                sb.Append(_allegries[i]);
+                if (i + 2 < _allegries.Count)
+                {
+                    sb.Append(", ");
+                }
+                if (i + 2 ==_allegries.Count)
+                {
+                    sb.Append(" and ");
+                }
+
+                if (i + 1 == _allegries.Count)
+                {
+                    sb.Append(".");
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        public bool IsAllergicTo(string allergen)
+        {
+            foreach (var allegry in _allegries)
+            {
+                if (allegry == allergen)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void AddAllergy(string allergen)
+        {
+            try
+            {
+                if (IsAllergicTo(allergen))
+                {
+                    return;
+                }
+                _allegries.Add(allergen);
+                _allegries = _allegries.OrderBy(x => GetScoreOfAllergen(x)).ToList();
+                _score += GetScoreOfAllergen(allergen);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public void DeleteAllergy(string allergen)
+        {
+            try
+            {
+                var index = _allegries.FindIndex(i => i == allergen); 
+                if (index >= 0) {
+                    _allegries.RemoveAt(index);
+                    _score -= GetScoreOfAllergen(allergen);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        private int CountScore()
+        {
+            try
+            {
+                int score = 0;
+                foreach (var allergen in _allegries)
+                {
+                    score += GetScoreOfAllergen(allergen);
+                }
+
+                return score;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        private List<string> ListOfAllergiesByString(string allergiesAsString)
+        {
+            try
+            {
+                var allegries = new List<string>();
+                string[] subs = allergiesAsString.Split(' ');
+                foreach (var sub in subs)
+                {
+                    allegries.Add(sub);
+                }
+                allegries = allegries.OrderBy(x => GetScoreOfAllergen(x)).ToList();
+                return allegries;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        private List<string> ListOfAllergiesByScore(int score)
+        {
+            try
+            {
+                var allegries = new List<string>();
+                int i = 0;
+                while(score > 0)
+                {
+                    var digit = score & 1;
+                    if (digit == 1)
+                    {
+                        string name = GetNameOfAllergen((int) Math.Pow(2, i));
+                        allegries.Add(name);
+                    }
+                    score = score >> 1;
+                    i++;
+                }
+
+                return allegries;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        private string GetNameOfAllergen(int allergen)
+        {
+            foreach(Allergen value in Enum.GetValues(typeof(Allergen)))
+            {
+                if ((int)value == allergen)
+                {
+                    return value.ToString();
+                }
+            }
+
+            throw new Exception("Error in GetNameOfAllergen: no allergen with score = " +  allergen);
+        }
+
+        private int GetScoreOfAllergen(string allergen)
+        {
+            foreach(Allergen value in Enum.GetValues(typeof(Allergen)))
+            {
+                string name = Enum.GetName(typeof(Allergen), value);
+                if (name == allergen)
+                {
+                    return (int)value;
+                }
+            }
+
+            throw new Exception("Error in GetScoreOfAllergen(): allergen " +  allergen + " do not exists");
+        }
+    }
+}
